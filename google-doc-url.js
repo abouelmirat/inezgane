df7f9f245b9ad6d458c62b4c4c0572c7349810f4
abouelmirat <abouelmirat@gmail.com>
2025-05-29 11:31:45 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t85Y26htxnyDPtgVyeAtL69i1K8uFHMOcLmStmMN484/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '';
+var googleApiKey = 'AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao';
